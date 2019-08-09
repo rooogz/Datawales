4a65670d38c57de9b418311ad6d7c4576e153998
roogz <ren_chao@rd.eastcom>
2019-08-09 17:09:45 +0800

    回家了，写了一半保存啊

---
+++ stack/static_CLassATTR.py
@@ -0,0 +1,16 @@
+class Test:
+	stri ="??"
+	def __init__(self):
+		self.name ="haha"
+		print(id(self.name))
+		print(id(self))
+
+
+t =Test()
+ 
+p =Test()
+
+
+
+# print(id(t.name))
+# print(id(p.name))

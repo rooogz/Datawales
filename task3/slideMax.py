ff0c456bf0c4467a7d0f19a9ada89ec9b01b782c
rooogz <rc-work@foxmail>
2019-08-11 23:08:20 +0800

    8-11 wrong

---
+++ task3/slideMax.py
@@ -0,0 +1,13 @@
+
+class MonotoneQueue:
+	def __init__
+
+class Solution:
+    def maxSlidingWindow(self, nums, k: int):
+    	t_max = nums[0]
+    	for i in range(len(nums)):
+    		if i < k:
+    			if nums[i] > t_max:
+    				t_max = e  
+    		else 
+    			

75a55abd5f975a2a7546d06bbdb1bed890afb4d1
rooogz <rc-work@foxmail>
2019-08-18 21:38:40 +0800

    del

--- task5/bst.cpp
+++ task5/bst.cpp
@@ -41,3 +41,57 @@ bool BSTree::insert(const int e)
 	return false;
 
 }
+bool BSTree::delete(const int e)
+{
+	BSTNode *p , *f;
+	if(!search(e, p, f))
+		return false;
+
+	if(!p->r)
+	{
+		if(f == NULL)
+			root = p>l;
+		else
+		{
+			if(f->l == p)
+				f->l = p->l;
+			else
+				f->r = p->l;
+		}
+		delete p;
+	}
+	else if(!p->l)
+	{
+		if(f == NULL)
+			root = p->r;
+		else
+		{
+
+			if(f->l == p)
+				f->l = p->r;
+			else
+				f->r = p->r;
+
+		}
+		delete p;
+
+	}
+	else
+	{
+		BSTNode * nn;
+		f= p;
+		nn = p->l;
+		while(nn->r)
+		{
+			f = nn;
+			nn = nn->r;
+		}
+		p->data = nn->data;
+		if(f != p)
+			f->r = nn->l;
+		else
+			f->l = s->l;
+		delete nn;
+	}
+	return true;
+}
